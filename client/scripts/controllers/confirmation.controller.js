31f9f5b986f3d4d7125f7320a2db0f5aad19e412
Wesley Lima <contato@wesley.pro.br>
2016-05-14 18:05:16 -0300

    Iniciado projeto

---
+++ client/scripts/controllers/confirmation.controller.js
@@ -0,0 +1,38 @@
+import { Controller } from '../entities';
+
+export default class ConfirmationCtrl extends Controller {
+  constructor() {
+    super(...arguments);
+
+    console.log('teste');
+
+    this.code = this.$state.params.code;
+    this.pass = this.$state.params.pass;
+  }
+
+  confirm() {
+
+    if (_.isEmpty(this.code) && _.isEmpty(this.pass)) return;
+
+    console.log( 'teste ');
+
+    Accounts.verifyPhone(this.code, this.pass, (err) => {
+      if (err) return this.handleError(err);
+      this.$state.go('profile');
+    });
+    this.$state.go('profile');
+  }
+
+  handleError(err) {
+
+    this.$log.error('Confirmation error ', err);
+
+    this.$ionicPopup.alert({
+      title: err.reason || 'Confirmation failed',
+      template: 'Please try again',
+      okType: 'button-positive button-clear'
+    });
+  }
+}
+
+ConfirmationCtrl.$inject = ['$scope', '$state', '$ionicPopup', '$log'];
---
+++ client/scripts/controllers/login.controller.js
@@ -0,0 +1,55 @@
+import { Controller } from '../entities';
+
+export default class LoginCtrl extends Controller {
+  login() {
+    if (_.isEmpty(this.code) || _.isEmpty(this.pass)) return;
+
+    // const confirmPopup = this.$ionicPopup.confirm({
+    //   title: 'Conectar',
+    //   template: '<div>' + this.phone + '</div><div>Sua senha esta correta?</div>',
+    //   cssClass: 'text-center',
+    //   okText: 'Sim',
+    //   okType: 'button-positive button-clear',
+    //   cancelText: 'Editar',
+    //   cancelType: 'button-dark button-clear'
+    // });
+
+    // confirmPopup.then((res) => {
+    //   if (!res) return;
+
+      this.$ionicLoading.show( {
+        template: 'Verificando conta...'
+      } );
+
+      if (_.isEmpty(this.code) && _.isEmpty(this.pass)) return;
+
+      console.log( 'teste ');
+
+      Accounts.verifyPhone(this.code, this.pass, (err) => {
+        if (err) return this.handleError(err);
+        this.$state.go('profile');
+      });
+      this.$state.go('profile');
+
+      // Accounts.requestPhoneVerification(this.code, (err) => {
+      //   this.$ionicLoading.hide();
+      //   if (err) return this.handleError(err);
+
+      //   console.log('confirmation');
+      //   this.$state.go('confirmation', { code: this.code });
+      // });
+    // });
+  }
+
+  handleError(err) {
+    this.$log.error('Falha ao conectar ', err);
+
+    this.$ionicPopup.alert({
+      title: err.reason || 'Falha no login',
+      template: 'Por favor tente novamente',
+      okType: 'button-positive button-clear'
+    });
+  }
+}
+
+LoginCtrl.$inject = ['$scope', '$state', '$ionicLoading', '$ionicPopup', '$log'];
